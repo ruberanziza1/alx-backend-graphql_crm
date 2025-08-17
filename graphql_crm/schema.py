799e6db3387dea58145fee53a6d191084f9ac663
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-17 11:36:46 +0200

    Update schema.py

--- graphql_crm/schema.py
+++ graphql_crm/schema.py
@@ -1,10 +1,4 @@
 import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
-
-class Query(CRMQuery, graphene.ObjectType):
-    pass
-
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
+from crm.schema import Query, Mutation
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
